d5b47ea519cc1ed06de0a494fb34ab26af9f63b2
Madge Florina Pereira <111832594+madgepereira020701@users.noreply.github.com>
2025-01-09 11:51:28 +0530

    Modified Home

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate from react-router-dom
 import './Home.css'; // Add your CSS file for styling
 
 const Home = () => {
   const navigate = useNavigate(); // Initialize navigate function
+  const [role, setRole] = useState(''); // Track user role
+
 
   const handleClick = () => {
     navigate('/members'); // Redirect to the Members page when button is clicked
   };
 
+
+  useEffect(() => {
+    const storedUserRole = localStorage.getItem('role');
+    if (storedUserRole) {
+      setRole(storedUserRole);
+    }
+  }, []);
+  
   return (
     <div className="home">
       <div className="home-overlay">
         <h1>Welcome to PulseFit</h1>
-        <p>Your one-stop solution for managing memberships and renewals.</p>
+        {role === 'Admin' ? (
+          <p>Your one-stop solution for managing memberships and renewals.</p>
+        ) : role === 'Member' ? (
+          <p>As a member, you have access to your details, view your payments, and calendar.</p>
+        ) : role === 'Employee' ? (
+          <p>As an employee, you have access to your details and calendar.</p>
+        ) : null}
+        
         <button className="home-button" onClick={handleClick}>
           Get Started
         </button>
