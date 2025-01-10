ca88cd290b572d6bb3d46c0603e28ef90f3975a3
Madge Florina Pereira <111832594+madgepereira020701@users.noreply.github.com>
2025-01-10 13:22:23 +0530

    Added attendance model

---
+++ backend/models/attendance.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const attendanceSchema = new mongoose.Schema({
+    in_time: {type: Date, required: true},
+    memno: {type: String, required: true},
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to User
+
+});
+
+const Attendance = mongoose.model('Attendance', attendanceSchema);
+
+module.exports = Attendance;
+
+
+
