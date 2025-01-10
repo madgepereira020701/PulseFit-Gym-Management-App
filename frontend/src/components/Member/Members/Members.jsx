f963514f5ed3aa550579880fd7fad4cc9d636e79
Madge Florina Pereira <111832594+madgepereira020701@users.noreply.github.com>
2025-01-10 13:53:11 +0530

    Modified it

--- frontend/src/components/Member/Members/Members.jsx
+++ frontend/src/components/Member/Members/Members.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Members.css';
 
@@ -14,9 +14,6 @@ const Members = () => {
     doj: '',
     doe: ''
   });
-  const [searchQuery, setSearchQuery] = useState(''); // Step 1: Add search query state
-  const searchInputRef = useRef(null); // Create a reference for the input field
-
 
   const navigate = useNavigate();
 
@@ -64,21 +61,6 @@ const Members = () => {
       doe: member.doe
     });
   };
-
-  // Step 2: Fix search handler and directly filter members
-  const handleSearch = (e) => {
-    let searchTerm = e.target.value.toLowerCase();
-    setSearchQuery(searchTerm);  // Store the lowercase query directly
-  };
-  
- 
-
-  const filteredMembers = members.filter((member) => {
-    return (
-      member.fullname.toLowerCase().includes(searchQuery) || 
-      member.email.toLowerCase().includes(searchQuery)
-    );
-  });
 
   const handleUpdateChange = (e) => {
     const { name, value } = e.target;
@@ -166,13 +148,6 @@ const Members = () => {
     navigate(`/payments/${memno}`);
   };
 
-  const handleIconClick = () => {
-    // Focus the input field when the search icon is clicked
-    if (searchInputRef.current) {
-      searchInputRef.current.focus();
-    }
-  };
-
   return (
     <div className="table-container">
       {loading ? (
@@ -182,21 +157,6 @@ const Members = () => {
       ) : (
         <>
           <h2>Members List</h2>
-          
-          <div className='search-bar-container'>
-            <input
-              type="text"
-              placeholder="Search members..."
-              className='search-bar'
-              value={searchQuery}
-              onChange={handleSearch}
-              ref={searchInputRef} // Attach ref to the input field
-            />
-            <i 
-              className="fas fa-search search-bar-icon"
-              onClick={handleIconClick} // Focus input field when clicked
-            ></i>
-          </div>
           <table className="member-table">
             <thead>
               <tr>
@@ -209,7 +169,7 @@ const Members = () => {
               </tr>
             </thead>
             <tbody>
-              {filteredMembers.map((member) => (
+              {members.map((member) => (
                 <tr key={member.memno}>
                   <td>{member.memno}</td>
                   <td>{member.fullname}</td>
