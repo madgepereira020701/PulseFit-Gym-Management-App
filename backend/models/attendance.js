f5f2bf09ac56cdde68222a999bb6021d1b03a88f
Madge Florina Pereira <111832594+madgepereira020701@users.noreply.github.com>
2025-01-14 16:57:46 +0530

    Mofdified attendance.js

--- backend/models/attendance.js
+++ backend/models/attendance.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 const attendanceSchema = new mongoose.Schema({
-    in_time: {type: Date, required: true},
+    date: {type: String, required: true},
+    in_time: {type: String, required: true},
+    out_time: {type: String, required: true},
     memno: {type: Number, required: true},
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Reference to User
 
