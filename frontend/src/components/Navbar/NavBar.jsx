4b38afb6b6a065c874ad77bab995d7344f8a83c9
Madge Florina Pereira <111832594+madgepereira020701@users.noreply.github.com>
2025-01-18 10:22:05 +0530

    Updated

--- frontend/src/components/Navbar/NavBar.jsx
+++ frontend/src/components/Navbar/NavBar.jsx
@@ -63,7 +63,7 @@ const Navbar = () => {
     <>
       <li><NavLink to="/home" activeClassName="active">Home</NavLink></li>
       <li><NavLink to="/details" activeClassName="active">Details</NavLink></li>
-      <li><NavLink to="/mcheck" activeClassName="active">Mark Attendance</NavLink></li>
+      <li><NavLink to="/check" activeClassName="active">Mark Attendance</NavLink></li>
       <li><NavLink to="/memberattendance" activeClassName="active">Attendance</NavLink></li>
       <li><NavLink to="/viewpayments" activeClassName="active">View Payments</NavLink></li>
       <li><NavLink to="/memcalendar" activeClassName="active">Calendar</NavLink></li>
@@ -72,7 +72,7 @@ const Navbar = () => {
     <>
       <li><NavLink to="/home" activeClassName="active">Home</NavLink></li>
       <li><NavLink to="/employeedetails" activeClassName="active">Details</NavLink></li>
-      <li><NavLink to="/echeck" activeClassName="active">Mark Attendance</NavLink></li>
+      <li><NavLink to="/check" activeClassName="active">Mark Attendance</NavLink></li>
       <li><NavLink to="/emcalendar" activeClassName="active">Calendar</NavLink></li>
     </>
   ) : null}
