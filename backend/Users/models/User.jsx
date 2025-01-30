288c7a9f60427d344ffc8b1a17eb339075c9d79a
Madge Florina Pereira <111832594+madgepereira020701@users.noreply.github.com>
2025-01-30 11:29:50 +0530

    Modified

--- backend/Users/models/User.jsx
+++ backend/Users/models/User.jsx
@@ -8,6 +8,8 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  passwordResetToken : String,
+  passwordResetExpires: Date
 });
 
 // Hash password before saving
