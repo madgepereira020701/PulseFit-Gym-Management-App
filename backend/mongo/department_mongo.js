5d57941968e4194c78dcec20448aaf40d29985c4
Madge Florina Pereira <111832594+madgepereira020701@users.noreply.github.com>
2025-02-20 15:17:29 +0530

    Added

---
+++ backend/mongo/department_mongo.js
@@ -0,0 +1,62 @@
+
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://gym:workout@cluster0.ie8js.mongodb.net/Gym-Admin?retryWrites=true&w=majority&appName=Cluster0';
+
+const addDepartments = async(req,res) => {
+    const newDepartment = {
+        department: req.body.department,
+        userId: req.user
+    };
+
+    const client = new MongoClient(url);
+
+    try{
+       await client.connect();
+       const db = client.db();
+       const result = await db.collection('departments').insertOne(newDepartment);
+       client.close();
+       res.status(201).json(result);
+    } catch (error) {
+        client.close();
+        res.status(500).json({ message: 'Could not store plan data.', error });
+    }
+};
+
+const getDepartments = async(req,res) => {
+    const client = new MongoClient(url);
+
+    try {
+        await client.connect();
+        const db = client.db();
+        const departments = await db.collection('departments').find({userId: req.user}).toArray();
+        client.close();
+        res.json(departments);
+    } catch (error) {
+        client.close();
+        res.status(500).json({ message: 'Could not retrieve departments.', error });
+    }
+};
+
+const deleteDepartment = async(req, res) => {
+    const department = req.params.department;
+    const client = new MongoClient(url);
+    
+    try {
+      await client.connect();
+      const db = client.db();
+      const result = await db.collection('departments').deleteOne({ department: department});
+      client.close();
+
+      if(result.deletedCount === 0) {
+        return res.status(404).json({messge: 'Department not found.'});
+      }
+
+      res.status(200).json({ message: 'Department deleted successfully.'});
+    } catch (error) {
+        client.close();
+      res.status(500).json({ message: 'Could not delete plan.', error });
+    }
+};
+
+module.exports = { addDepartments, getDepartments, deleteDepartment };
+
