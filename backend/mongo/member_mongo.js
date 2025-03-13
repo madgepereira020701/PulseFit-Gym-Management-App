ba3d31b4189bcfe121747bc088bac63989f00aa4
Madge Florina Pereira <111832594+madgepereira020701@users.noreply.github.com>
2025-03-13 12:33:10 +0530

    Update member_mongo.js

--- backend/mongo/member_mongo.js
+++ backend/mongo/member_mongo.js
@@ -27,7 +27,7 @@ transporter.verify((error) => {
 // Utility function to send email
 const sendEmail = async (to, subject, htmlContent) => {
   const mailOptions = {
-    from: 'madgepereira020701@gmail.com',
+    from: '', //Your email
     to,
     subject,
     html: htmlContent,
