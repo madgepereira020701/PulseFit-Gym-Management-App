d41d352943635ed8ef615e88b6047b61383140ce
Madge Florina Pereira <111832594+madgepereira020701@users.noreply.github.com>
2025-03-13 12:26:52 +0530

    Update emmiddleware.js

--- backend/Users/middlewares/emmiddleware.js
+++ backend/Users/middlewares/emmiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET = 'mysecretkey';
+const JWT_SECRET = ''; //yoursecretkey (any name)
 
 // Middleware to verify token
 const protect2 = async (req, res, next) => {
