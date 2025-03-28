474c5371d27f11175ec61c763603fc28dd03fab2
Madge Florina Pereira <111832594+madgepereira020701@users.noreply.github.com>
2025-03-28 18:20:25 +0530

    Update department_mongo.js

--- backend/mongo/department_mongo.js
+++ backend/mongo/department_mongo.js
@@ -1,7 +1,7 @@
 
 const MongoClient = require('mongodb').MongoClient;
 // for the below url, paste the mongodb uri link 
-const url = //mongodb uri link;
+const url = 'mongodb uri link';
 
 
 const addDepartments = async(req,res) => {
